a672252569ddf33f526522bba43fe45d4db84518
jxleyo.HRP <dyhrp@hotmail.com>
2022-06-21 17:08:05 +0800

    Add files via upload

---
+++ MltpSvc0.1/resource.h
@@ -0,0 +1,52 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MltpSvc.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_MltpSvc_DIALOG 102
+#define IDS_APP_TITLE                   103
+#define IDI_SMALL                       108
+#define IDR_MAINFRAME                   128
+#define IDR_TRAYMENU                    130
+#define IDD_DIALOG_REG                  133
+#define IDB_JPG_SKETCHCN                135
+#define IDB_JPG_SKETCHEN                136
+#define IDB_PNG_ICONSMALL               139
+#define WM_SYSTEMTRAY                   503
+#define IDC_STATIC_VER                  1001
+#define IDC_STATIC_COPYRIGHT            1002
+#define IDC_STATIC_ICONSMALL            1003
+#define IDC_SYSLINK_WEBSITE             1004
+#define IDC_REGISTRY                    1012
+#define IDC_BUTTON_REG                  1012
+#define IDC_STATIC_SKETCHTITLE          1013
+#define IDC_STATIC_SKETCHIMAGECONTAINER 1014
+#define IDC_STATIC_SKETCHIMAGE          1015
+#define IDC_STATIC_PAY                  1016
+#define IDC_STATIC_ICONSMAL             1016
+#define IDC_STATIC_PAYAL                1017
+#define IDC_STATIC_REGSN                1019
+#define IDC_STATIC_REG                  1020
+#define IDC_EDIT_REGINPUT               1021
+#define IDC_BUTTON_REGOK                1022
+#define IDC_STATIC_REGINFO              1023
+#define IDC_STATIC_REGMETHOD            1023
+#define IDC_STATIC_REGMETHODINFO        1024
+#define IDC_SYSLINK_EULA                1025
+#define IDC_SYSLINK1                    1026
+#define IDC_SYSLINK_VIDEOTUTOR          1026
+#define IDM_EXIT                        32776
+#define ID_MANUAL                       32784
+#define ID_VIDEOTUTOR                   32786
+#define ID_ABOUT                        32787
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        140
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
