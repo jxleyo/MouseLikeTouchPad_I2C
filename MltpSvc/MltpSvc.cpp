01e5855117411412d63a0c1f6ec1704c3d22d475
jxleyo.HRP <dyhrp@hotmail.com>
2022-06-22 12:01:48 +0800

    Add files via upload

--- MltpSvc/MltpSvc.cpp
+++ MltpSvc/MltpSvc.cpp
@@ -79,7 +79,7 @@ BOOL CMltpSvcApp::InitInstance()
 	// 更改用于存储设置的注册表项
 	// TODO: 应适当修改该字符串，
 	// 例如修改为公司或组织名
-	SetRegistryKey(_T("应用程序向导生成的本地应用程序"));
+	SetRegistryKey(_T("MouseLikeTouchPad"));//注册表项
 
 	CMltpSvcDlg dlg;
 	m_pMainWnd = &dlg;
